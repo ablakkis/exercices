faed884a4b73122fd1e46bfb01d4a1bc62bb092a
ablakkis <abslakkis@outlook.fr>
2022-10-24 11:00:47 -0400

    version avant final

--- revuepair.py
+++ revuepair.py
@@ -7,7 +7,7 @@ def addition_fraction(fraction_1,fraction_2):
 def soustraction_fraction(fraction_1, fraction_2):
     a, b = fraction_2
     a, b = -a, -b
-    return addition_fraction(fraction1, (a, b))
+    return addition_fraction(fraction_1, (a, b))
 def multiplication_fraction(fraction_1, fraction_2):
     a, b = fraction_1
     c, d= fraction_2
@@ -16,12 +16,20 @@ def division_fraction(fraction_1, fraction_2):
     a, b = fraction_2
     a, b = b, a
     return multiplication_fraction((a,b))  
-
 def fraction_to_str(fraction):
     a, b = fraction
-    return str(a) + '/' + b +' '
+    return str(a) + '/' + str(b) +':'
 def affiche_fraction(fraction):
     a, b = fraction
-    print((fraction_to_str(fraction), a/b))
-affiche_fraction((3,2)) 
+    print((fraction_to_str(fraction) + str(a/b)))
+fraction_result = addition_fraction((3,5),(4,6))
+affiche_fraction(fraction_result)
+fraction_result = soustraction_fraction((3,5),(4,6))
+affiche_fraction(fraction_result)
+fraction_result= multiplication_fraction((3,5),(4,6))
+affiche_fraction(fraction_result)
+fraction_result = division_fraction((3,5),(4,6))
+affiche_fraction(fraction_result)
+
+
    
