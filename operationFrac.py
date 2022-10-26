81bca9429686942aacd27c8563729c6b90b33ba4
ablakkis <abslakkis@outlook.fr>
2022-10-26 13:12:45 -0400

    version finale

--- operationFrac.py
+++ operationFrac.py
@@ -4,24 +4,28 @@ def addition_fraction(fraction_1,fraction_2):
     num = a*d + b*c
     denom = b*d
     return num, denom
+
 def soustraction_fraction(fraction_1, fraction_2):
     a, b = fraction_2
     a, b = -a, -b
-    return addition_fraction(fraction1, (a, b))
+    return addition_fraction(fraction_1, (a, b))
+
 def multiplication_fraction(fraction_1, fraction_2):
     a, b = fraction_1
     c, d= fraction_2
-    return a*b, c*d
+    return a*c, b*d
+
 def division_fraction(fraction_1, fraction_2):
     a, b = fraction_2
     a, b = b, a
     return multiplication_fraction((a,b))  
-
+    
 def fraction_to_str(fraction):
     a, b = fraction
-    return str(a) + '/' + b +' '
+    return str(a) + '/' + str(b) +': '
 def affiche_fraction(fraction):
     a, b = fraction
     print((fraction_to_str(fraction), a/b))
+    
 affiche_fraction((3,2)) 
    
