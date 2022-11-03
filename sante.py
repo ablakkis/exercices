0b6bac26f67085f7d905b50815366bacfed076b2
ablakkis <116194019+ablakkis@users.noreply.github.com>
2022-11-03 16:25:50 -0400

    IMC avec IMC version 2013

--- sante.py
+++ sante.py
@@ -7,7 +7,13 @@ def calcule_imc (poids, taille) :
         return poids / taille**2
     else :
         return 0;
-     
+
+def calcule_imc2013(poids, taille):
+    if taille > 0:
+            return 1.3 * poids / taille**2.5
+    else :
+        return 0;
+
 def  message_correspondant_resultat_imc(imc) :
     if imc < 20:
         return "Problemes de sante ou peut etre une mauvaise nutrition"
@@ -18,11 +24,30 @@ def  message_correspondant_resultat_imc(imc) :
     else :
         return "Un risque accru de probleme de sante"
 
+def  message_correspondant_resultat_imc2013(imc) :
+    if imc <= 15.5 :
+        return "Maigreur severe ou anorexie"
+    if  15.5 < imc <= 17.49:
+        return "Insuffisance ponderale visible ou anorexie moderee"
+    if 17.5 <= imc <= 18.49:
+        return "Insuffisance pondderale legere"
+    if 18.5 <= imc <= 24.9 :
+        return "corpulence normale (poids ideal)"
+    if 25 <= imc <= 29.9 :
+        return "surpoids"
+    if 30 <= imc <= 34.9 :
+        return "Obesite de  classe I ou modere"
+    if 35 <= imc <= 39.9 :
+        return "Obesite de  classe II"
+    else :
+        return "Obesite de  classe III"
+
 def main():
     poids = float(input("Entrer votre poids en Kg:" ))
     taille = float(input("Entrer votre taille en metres:" ))
-    imc = calcule_imc (poids, taille) 
-    resultat =  message_correspondant_resultat_imc(imc)
+    #imc = calcule_imc (poids, taille) 
+    imc = calcule_imc2013 (poids, taille) 
+    resultat =  message_correspondant_resultat_imc2013 (imc)
     print(f"IMC: {imc:.3} -> {resultat}")
 
 main()
