afbe15552b4b99f1403e4d6d81f86b6991d9a172
ablakkis <116194019+ablakkis@users.noreply.github.com>
2022-11-09 05:44:37 -0500

    version optimiser

--- trie.py
+++ trie.py
@@ -1,18 +1,20 @@
 #trier 4 entiers sans utilisation de boucle
 
 def permut(a, b):
+
     if(a > b):
-        a, b = b, a
-    return a, b
+        return b, a, True
+    return a, b, False
 
 def arranger(a1, a2, a3, a4, n ): # n étant le nombre de comparaison a faire a partir du début
+    ok = False
     if n >= 1:
-        a1, a2 = permut(a1, a2)
+        a1, a2, ok = permut(a1, a2)
     if(n >= 2):
-        a2, a3 = permut(a2, a3)
+        a2, a3, ok = permut(a2, a3)
     if(n == 3):
-        a3, a4 = permut(a3, a4) 
-    return a1, a2, a3, a4
+        a3, a4 , ok = permut(a3, a4) 
+    return a1, a2, a3, a4, ok
 
 def saisie_suite():
     a1 = int(input("entrer le premier entier: "))
@@ -23,9 +25,11 @@ def saisie_suite():
 
 def go():
     a1, a2, a3, a4 = saisie_suite()
-    a1, a2, a3, a4 = arranger(a1, a2, a3, a4, 3)
-    a1, a2, a3, a4 = arranger(a1, a2, a3, a4, 2)
-    a1, a2, a3, a4 = arranger(a1, a2, a3, a4,1)
+    a1, a2, a3, a4, ok = arranger(a1, a2, a3, a4, 3)
+    if ok :
+        a1, a2, a3, a4, ok = arranger(a1, a2, a3, a4, 2)
+        if ok :
+            a1, a2, a3, a4, ok = arranger(a1, a2, a3, a4,1)
     print(f"suite triée {a1}, {a2}, {a3}, {a4}")
 go()
 
