dfda9c0fe41877199db8a582d5082eca1c3e531e
ablakkis <116194019+ablakkis@users.noreply.github.com>
2022-11-11 22:13:40 -0500

    si nombre premier

---
+++ jeu.py
@@ -0,0 +1,25 @@
+import random 
+
+def jouer():
+    limite = int(input("Limit: "))
+    nb_essais = int(input("Nombre d'essais: "))
+    nombre_a_deviner = random.randint(1,limite)
+    i = 1
+    trouve = False
+    while i <= nb_essais and not trouve:
+        choix_utilisateur = int(input("Votre proposition: "))
+        if choix_utilisateur == nombre_a_deviner:
+            print(f"Gagner aprés {i} tentatives")
+            trouve = True
+        elif  choix_utilisateur < nombre_a_deviner :
+            print("Votre nombre est trop petit")
+            i = i + 1
+        elif  choix_utilisateur > nombre_a_deviner: 
+            print("Votre nombre est trop grand")
+            i = i + 1
+    if not trouve:
+        print(f"Vous avez perdu, le nombre à deviner était {nombre_a_deviner}")   
+
+jouer()
+        
+
