3e625985513bb56d829ff7c83f698e35aa3361c2
ablakkis <116194019+ablakkis@users.noreply.github.com>
2022-11-25 16:31:18 -0500

    debut

--- copyfichier.py
+++ copyfichier.py
@@ -1,28 +1,6 @@
 
-def lire_fichier_determinants():
+def construite_liste_determinants_from_file():
     with open("determinants.txt", "r", encoding = "utf8") as fin:
         contenu = fin.read()
+        return contenu.strip(",")
     return contenu
-
-def construire_tuple_determinants_marqueurs_relation(contenu: str):
-    liste_determinants = contenu.strip(",")
-    determinants = tuple(liste_determinants)
-    return determinants
-
-def proceed():
-    determs = lire_fichier_determinants()
-    liste_determinants = determs.strip(",")
-    tupl = construire_tuple_determinants_marqueurs_relation(liste_determinants)
-    if("d'abort" in liste_determinants):
-        print("ok")
-    else:
-        print("non ok")
-
-    #for t in tupl:
-     #   print(t, end = " ")
-    #print(tupl)
-
-proceed()
-
-
-
