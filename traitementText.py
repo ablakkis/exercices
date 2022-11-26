f865d88aca86492b43e7c02bbcc3968d81d38e95
ablakkis <116194019+ablakkis@users.noreply.github.com>
2022-11-26 08:49:01 -0500

    changement

--- traitementText.py
+++ traitementText.py
@@ -1,38 +1,45 @@
-def construite_liste_determinants_from_file(file: str): 
+import re
+def construire_liste_determinants_from_file(file: str): 
     with open(file, "r", encoding = "utf8") as fin:
         contenu = fin.read()
-        return contenu.strip(",")
-def construire_liste_mots_du_fichier(file: str):
-    separateurs = ".,;?!: #/{}[]-_+=()@$\"\n\t{%\\"
-    with open(file, "r", encoding = "utf8") as fin:
-        contenu = fin.read()
-        contenu = contenu.lower()
-        liste_mots = contenu.strip(separateurs)
-        return liste_mots
-
-def si_apostrophe_a_enlever(mot: str):
-    n = mot.index("'")
-    if n == 1:
-        return True
-    else:
-        return False
+        return contenu.rsplit(",")
+def construire_lignes_du_fichier(file: str):
     
-def construire_dictionnaire_compteur(dictionnaire: dict, contenu_file: list,  determinants: list):
+    with open(file, "r", encoding = "utf8") as fin:
+        lines = fin.readlines()
+        for line in lines:
+            line = line[:-1]
+            line = line.lower()
+        return lines
 
+def split_line_update_dictionnaire(dictionnaire: dict, line: str, sep: str, determinants: list):
     def ajout_mot_dictionnaire(dictionnaire: dict, mot: str):
         if mot in dictionnaire:
             dictionnaire[mot] += 1
         else:
             dictionnaire[mot] = 1 
-    
-    for mot in contenu_file:
+
+    def si_apostrophe_a_enlever(mot: str):
+        n = mot.find("\'")
+        if n == 1:
+            return True
+        else:
+            return False
+
+    liste_mots = line.rsplit(sep)
+    print(liste_mots)
+    for mot in liste_mots:
         if mot not in determinants:
             if si_apostrophe_a_enlever(mot):
                 if(len(mot) > 2):
                     mot = mot[2:]
-                    ajout_mot_dictionnaire(dictionnaire, mot)
-            else:
-                ajout_mot_dictionnaire(dictionnaire, mot)  
+            ajout_mot_dictionnaire(dictionnaire, mot)
+      
+def construire_dictionnaire_compteur(dictionnaire: dict, lines: list,  determinants: list, sep: str):
+
+    for line in lines:
+        split_line_update_dictionnaire(dictionnaire, line, sep, determinants)
+
 
 def calcul_probabilite_occurence_mots(dictionnaire: dict):
     liste_cles = list(dictionnaire.keys())
@@ -46,20 +53,24 @@ def mot_probabilite_max(dictionnaire: dict):
     for k in liste_cles:
         if max < dictionnaire[k]:
             max, cle = dictionnaire[k], k
-    return k 
+    return cle 
 
 def traitement():
-
+    separateurs = ".,;?!: #/{}[]-_+=()@$\n\t%\"\—«»"
     dictionnaire : dict
     dictionnaire = {}
-    list_determinants_marq_relation = construite_liste_determinants_from_file("determinants.txt")
-    liste_mots_texte = construire_liste_mots_du_fichier("texte.txt")
-    construire_dictionnaire_compteur(dictionnaire, liste_mots_texte, list_determinants_marq_relation)
+    list_determinants_marq_relation = construire_liste_determinants_from_file("determinants.txt")
+    liste_lignes = construire_lignes_du_fichier("texte.txt")
+    construire_dictionnaire_compteur(dictionnaire, liste_lignes, list_determinants_marq_relation, separateurs)
     calcul_probabilite_occurence_mots(dictionnaire)
     mot_occurence_max = mot_probabilite_max(dictionnaire)
     print(f"Il y a une tres grande chance que le texte concerne le sujet de {mot_occurence_max}")
 
-traitement()
+#traitement()
+if __name__ == '__main__':
+    separateurs = ".,;?!: #/{}[]-_+=()@$\n\t%\"\—«»"
+    res = re.split(separateurs, "je, suis, abbas. j'aime ma femme(salwa) mais aussi [sajed]")
+print(res)
 
 
 
